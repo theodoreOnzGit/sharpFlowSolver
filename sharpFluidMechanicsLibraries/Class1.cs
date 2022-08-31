6fb61332bfa1712409acd226c328af33d386d442
teddy0@archDesktop <tong011@e.ntu.edu.sg>
2022-08-30 17:10:23 -0700

    updated licensing

--- sharpFluidMechanicsLibraries/Class1.cs
+++ sharpFluidMechanicsLibraries/Class1.cs
@@ -1,4 +1,6 @@
 ﻿namespace sharpFluidMechanicsLibraries{
+	// the end user only need look at the following class to see what
+	// functionality is available in this code.
 	public class Class1
 	{
 
